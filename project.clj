ebc8686eef023ac1682ea8a27a028646775babae
Lih Chen <lihster@gmail.com>
2020-02-23 18:04:28 -0800

    Read in a csv file

--- project.clj
+++ project.clj
@@ -10,7 +10,8 @@
                  [prismatic/dommy "1.1.0"]
                  [testdouble/clojurescript.csv "0.4.5"]
                  [domina "1.0.3"]
-
+                 [reagent "0.8.1"]
+                 [re-com "2.6.0"]
                  [figwheel "0.5.19"]
                  [environ "1.1.0"]]
 
--- src/popup/google_webmaster_tools_bulk_outdated_content_removal_clj/popup/core.cljs
+++ src/popup/google_webmaster_tools_bulk_outdated_content_removal_clj/popup/core.cljs
@@ -1,9 +1,24 @@
 (ns google-webmaster-tools-bulk-outdated-content-removal-clj.popup.core
   (:require-macros [cljs.core.async.macros :refer [go-loop]])
-  (:require [cljs.core.async :refer [<!]]
+  (:require [cljs.core.async :refer [<! >! put! chan] :as async]
             [chromex.logging :refer-macros [log info warn error group group-end]]
             [chromex.protocols.chrome-port :refer [post-message!]]
+            [reagent.core :as reagent :refer [atom]]
+            [re-com.core :as recom]
+            [testdouble.cljs.csv :as csv]
+            [domina :refer [single-node nodes]]
+            [domina.xpath :refer [xpath]]
             [chromex.ext.runtime :as runtime :refer-macros [connect]]))
+
+(def upload-chan (chan 1 (map (fn [e]
+                                (let [target (.-currentTarget e)
+                                      file (-> target .-files (aget 0))]
+                                  (set! (.-value target) "")
+                                  file
+                                  )))))
+
+(def read-chan (chan 1 (map #(-> % .-target .-result js->clj))))
+
 
 ; -- a message loop ---------------------------------------------------------------------------------------------------------
 
@@ -18,13 +33,80 @@
       (recur))
     (log "POPUP: leaving message loop")))
 
-(defn connect-to-background-page! []
-  (let [background-port (runtime/connect)]
-    (post-message! background-port "hello from POPUP!")
-    (run-message-loop! background-port)))
+(defn connect-to-background-page! [background-port]
+  (post-message! background-port "hello from POPUP!")
+  (run-message-loop! background-port))
+
+
+(defn current-page []
+  (let []
+    [recom/v-box
+     :width "380px"
+     :align :center
+     :children [[:div {:style {:display "none"}}
+                 [:input {:id "bulkCsvFileInput" :type "file"
+                          :on-change (fn [e]
+                                       ;; (reset! my-status :running)
+                                       ;; (reset! cached-bad-victims-atom nil)
+                                       (put! upload-chan e))}]]
+                [recom/v-box
+                 :align :start
+                 :style {:padding "10px"}
+                 :children [[recom/title :label "Instructions:" :level :level1]
+                            [recom/label :label "- more instructions go here"]]
+                 ]
+                [recom/v-box
+                 :gap "10px"
+                 :children [[recom/button
+                             :label "Submit CSV File"
+                             :tooltip [recom/v-box
+                                       :children [[recom/label :label "Tooltip goes here"]]
+                                       ]
+                             :style {:width "200px"
+                                     :background-color "#007bff"
+                                     :color "white"}
+                             :on-click (fn [e] (-> "//input[@id='bulkCsvFileInput']" xpath single-node .click))
+
+                             ]
+                            ;; TODO: do we need clear cache and view cache?
+                            ]
+                 ]]
+     ]))
+
+(defn mount-root []
+  (reagent/render [current-page] (.getElementById js/document "app")))
 
 ; -- main entry point -------------------------------------------------------------------------------------------------------
 
+
 (defn init! []
-  (log "POPUP: init")
-  (connect-to-background-page!))
+  (let [_ (log "POPUP: init")
+        background-port (runtime/connect)]
+    ;; handle onload
+    (go-loop []
+      (let [reader (js/FileReader.)
+            file (<! upload-chan)]
+        (set! (.-onload reader) #(put! read-chan %))
+        (.readAsText reader file)
+        (recur)))
+
+    ;; handle reading the file
+    (go-loop []
+      (let [file-content (<! read-chan)
+            _ (prn "file-content: " (clojure.string/trim file-content))
+            csv-data (->> (csv/read-csv (clojure.string/trim file-content))
+                          ;; trim off random whitespaces
+                          (map (fn [[url method url-type]]
+                                 (->> [(clojure.string/trim url)
+                                       (when method (clojure.string/trim method))
+                                       (when url-type (clojure.string/trim url-type))]
+                                      (filter (complement nil?))
+                                      ))))]
+        (log "about to call :init-victims")
+        ;; (post-message! background-port (common/marshall {:type :init-victims
+        ;;                                                  :data csv-data
+        ;;                                                  }))
+        (recur)))
+
+    (connect-to-background-page! background-port)
+    (mount-root)))
