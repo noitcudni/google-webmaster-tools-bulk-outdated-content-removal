1bafb01da900bdf8ee6efef7ed6c447bc8e341dd
Lih Chen <lihster@gmail.com>
2020-03-29 14:59:49 -0700

    Able to submit the next victim

--- src/content_script/google_webmaster_tools_bulk_outdated_content_removal_clj/content_script/core.cljs
+++ src/content_script/google_webmaster_tools_bulk_outdated_content_removal_clj/content_script/core.cljs
@@ -8,6 +8,7 @@
             [domina :refer [single-node nodes style styles]]
             [domina.xpath :refer [xpath]]
             [dommy.core :refer-macros [sel sel1] :as dommy]
+            [google-webmaster-tools-bulk-outdated-content-removal-clj.background.storage :refer [update-storage]]
             ))
 
 (defn sync-node-helper
@@ -50,82 +51,106 @@
 
 (defn exec-removal-request
   [victim-url supplementary-arg]
-  (let [ch (chan)]
-    (go
-      (let [
-            ;; Fill out the input field
-            _ (domina/set-value! (-> "//input[contains(@class,'gwt-TextBox')]" xpath single-node)
-                                 victim-url)
-            ;; click on the Request Removal button
-            _ (-> "//button/div[contains(text(), 'Request Removal')]" xpath single-node .click)
-            dialog (<! (sync-single-node
-                        "//div[@role='dialog']//*[contains(text(), 'Now you can submit your temporary removal request.')]"
-                        "//div[@role='dialog']//*[contains(text(), 'The URL you want to remove is:')]"
-                        "//div[@role='dialog']//*[contains(text(), 'The image you want to remove is:')]"))]
-        (cond
-          ;; case 1
-          (-> "//div[@role='dialog']//*[contains(text(), 'Now you can submit your temporary removal request.')]"
-              xpath
-              single-node)
-          (do (prn "Now you can submit your temporary removal request.")
+  (when-not (nil? victim-url)
+    (let [ch (chan)]
+      (go
+        (let [
+              ;; wait for the button to be loaded
+              _ (<! (sync-single-node "//button/div[contains(text(), 'Request Removal')]"))
+              ;; Fill out the input field
+
+              _ (domina/set-value! (-> "//input[contains(@class,'gwt-TextBox')]" xpath single-node)
+                                   victim-url)
+              ;; click on the Request Removal button
+              _ (-> "//button/div[contains(text(), 'Request Removal')]" xpath single-node .click)
+              dialog (<! (sync-single-node
+                          "//div[@role='dialog']//*[contains(text(), 'Now you can submit your temporary removal request.')]"
+                          "//div[@role='dialog']//*[contains(text(), 'The URL you want to remove is:')]"
+                          "//div[@role='dialog']//*[contains(text(), 'The image you want to remove is:')]"
+                          "//div[@role='dialog']//*[contains(text(), 'This content is no longer live on the website.')]"
+                          ))]
+          (cond
+            ;; case 1
+            (-> "//div[@role='dialog']//*[contains(text(), 'Now you can submit your temporary removal request.')]"
+                xpath
+                single-node)
+            (do (prn "Now you can submit your temporary removal request.")
+                (.click (<! (sync-single-node "//div[@role='dialog']//button//div[contains(text(), 'Request Removal')]")))
+                (.click (<! (sync-single-node "//div[@role='dialog']//button//div[contains(text(), 'OK')]")))
+                (<! (update-storage victim-url "status" "removed"))
+                (>! ch :success))
+
+            ;; case 2
+            (-> "//div[@role='dialog']//*[contains(text(), 'The URL you want to remove is:')]"
+                xpath
+                single-node)
+            (do (prn "The URL you want to remove is")
+                (.click (<! (sync-single-node "//div[@role='dialog']//button//div[contains(text(), 'Next')]")))
+                (.click (<! (sync-single-node "//div[@role='dialog']//button//div[contains(text(), 'Next')]")))
+                (<! (sync-single-node "//div[@role='dialog']//input[@placeholder='Enter one word here...']"))
+                ;; TODO: how to wait for user action. Does the page refresh?
+                (when-not (empty? supplementary-arg)
+                  (-> "//div[@role='dialog']//input[@placeholder='Enter one word here...']"
+                      xpath
+                      single-node
+                      (domina/set-value! supplementary-arg))
+                  (.click (<! (sync-single-node "//div[@role='dialog']//button//div[contains(text(), 'Request Removal')]")))
+                  (.click (<! (sync-single-node "//div[@role='dialog']//button//div[contains(text(), 'OK')]")))
+                  (<! (update-storage victim-url "status" "removed"))
+                  (>! ch :success)
+                  ))
+
+            ;; case 3
+            (-> "//div[@role='dialog']//*[contains(text(), 'The image you want to remove is:')]"
+                xpath
+                single-node)
+            (do (prn  "The image you want to remove is:")
+                (.click (<! (sync-single-node "//div[@role='dialog']//button//div[contains(text(), 'Next')]")))
+                (<! (sync-single-node "//div[@role='dialog']//input[@placeholder='Example URL: https://www.google.com/url?url=http://www.example.com/oldpage']"))
+                (when-not (empty? supplementary-arg)
+                  ;; TODO: The button isn't lit up!
+                  (let [key-evt (js/document.createEvent "KeyboardEvent")
+                        evt (js/document.createEvent "HTMLEvents")
+                        _ (.initEvent evt "keyup" true false)
+                        _ (aset evt "keyCode" 13)
+                        input-el (-> "//div[@role='dialog']//input[@placeholder='Example URL: https://www.google.com/url?url=http://www.example.com/oldpage']"
+                                     xpath
+                                     single-node)]
+                    (domina/set-value! input-el supplementary-arg)
+                    ;; need to simulate a keyup event in order to make the Request Removal button clickable.
+                    (.dispatchEvent input-el evt))
+
+                  (.click (<! (sync-single-node "//div[@role='dialog']//button//div[contains(text(), 'Request Removal')]")))
+                  (.click (<! (sync-single-node "//div[@role='dialog']//button//div[contains(text(), 'OK')]")))
+                  (<! (update-storage victim-url "status" "removed"))
+                  (>! ch :success)
+                  ))
+
+            ;;case 4
+            (-> "//div[@role='dialog']//*[contains(text(), 'This content is no longer live on the website.')]"
+                xpath
+                single-node)
+            (do
               (.click (<! (sync-single-node "//div[@role='dialog']//button//div[contains(text(), 'Request Removal')]")))
               (.click (<! (sync-single-node "//div[@role='dialog']//button//div[contains(text(), 'OK')]")))
-              (>! ch :success))
-
-          ;; case 2
-          (-> "//div[@role='dialog']//*[contains(text(), 'The URL you want to remove is:')]"
-              xpath
-              single-node)
-          (do (prn "The URL you want to remove is")
-              (.click (<! (sync-single-node "//div[@role='dialog']//button//div[contains(text(), 'Next')]")))
-              (.click (<! (sync-single-node "//div[@role='dialog']//button//div[contains(text(), 'Next')]")))
-              (<! (sync-single-node "//div[@role='dialog']//input[@placeholder='Enter one word here...']"))
-              ;; TODO: how to wait for user action. Does the page refresh?
-              (when-not (empty? supplementary-arg)
-                (-> "//div[@role='dialog']//input[@placeholder='Enter one word here...']"
-                    xpath
-                    single-node
-                    (domina/set-value! supplementary-arg))
-                (.click (<! (sync-single-node "//div[@role='dialog']//button//div[contains(text(), 'Request Removal')]")))
-                (.click (<! (sync-single-node "//div[@role='dialog']//button//div[contains(text(), 'OK')]")))
-                (>! ch :success)
-                ))
-
-          ;; case 3
-          (-> "//div[@role='dialog']//*[contains(text(), 'The image you want to remove is:')]"
-              xpath
-              single-node)
-          (do (prn  "The image you want to remove is:")
-              (.click (<! (sync-single-node "//div[@role='dialog']//button//div[contains(text(), 'Next')]")))
-              (<! (sync-single-node "//div[@role='dialog']//input[@placeholder='Example URL: https://www.google.com/url?url=http://www.example.com/oldpage']"))
-              (when-not (empty? supplementary-arg)
-                ;; TODO: The button isn't lit up!
-                (let [key-evt (js/document.createEvent "KeyboardEvent")
-                      evt (js/document.createEvent "HTMLEvents")
-                      _ (.initEvent evt "keyup" true false)
-                      _ (aset evt "keyCode" 13)
-                      input-el (-> "//div[@role='dialog']//input[@placeholder='Example URL: https://www.google.com/url?url=http://www.example.com/oldpage']"
-                                   xpath
-                                   single-node)]
-                  (domina/set-value! input-el supplementary-arg)
-                  ;; need to simulate a keyup event in order to make the Request Removal button clickable.
-                  (.dispatchEvent input-el evt))
-
-                (.click (<! (sync-single-node "//div[@role='dialog']//button//div[contains(text(), 'Request Removal')]")))
-                (.click (<! (sync-single-node "//div[@role='dialog']//button//div[contains(text(), 'OK')]")))
-                (>! ch :success)
-                ))
-          )
-        ))))
+              (<! (update-storage victim-url "status" "removed")))
+            )
+          ))
+      ch ;;TODO: channel is probably not needed here since the page refreshes.
+      )))
 
 ; -- a message loop ---------------------------------------------------------------------------------------------------------
 
 (defn process-message! [chan message]
   (let [_ (log "CONTENT SCRIPT: got message:" message)
         {:keys [type victim supplementary-arg] :as whole-msg} (common/unmarshall message)]
-    (cond (= type :done-init-victims) (post-message! chan (common/marshall {:type :next-victim}))
+    (cond (= type :done-init-victims) (do
+                                        (prn "handling done-init-victims ...")
+                                        (post-message! chan (common/marshall {:type :next-victim})))
           (= type :remove-url) (do (prn "handling removel-url") ;;xxx
-                                   (exec-removal-request victim supplementary-arg)
+                                   (if (= victim "poison-pill")
+                                     (js/alert "All done!")
+                                     (exec-removal-request victim supplementary-arg))
                                    )
           )))
 
@@ -134,5 +159,8 @@
 (defn init! []
   (let [_ (log "CONTENT SCRIPT: init")
         background-port (runtime/connect)]
+    ;; since remove outdated content does a page reload,
+    ;; we need to ask the backgrounmd for next victim
+    (post-message! background-port (common/marshall {:type :next-victim}))
 
     (common/connect-to-background-page! background-port process-message!)))
