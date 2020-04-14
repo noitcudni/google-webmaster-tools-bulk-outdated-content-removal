408e306994ce44c979d4f0a43cbe748fc66b25ec
Lih Chen <lihster@gmail.com>
2020-04-13 21:07:40 -0700

    Have the error related components in place

--- src/popup/google_webmaster_tools_bulk_outdated_content_removal_clj/popup/core.cljs
+++ src/popup/google_webmaster_tools_bulk_outdated_content_removal_clj/popup/core.cljs
@@ -1,5 +1,6 @@
 (ns google-webmaster-tools-bulk-outdated-content-removal-clj.popup.core
-  (:require-macros [cljs.core.async.macros :refer [go-loop]])
+  (:require-macros [cljs.core.async.macros :refer [go-loop]]
+                   [reagent.ratom :refer [reaction]])
   (:require [cljs.core.async :refer [<! >! put! chan] :as async]
             [chromex.logging :refer-macros [log info warn error group group-end]]
             [chromex.protocols.chrome-port :refer [post-message!]]
@@ -10,8 +11,12 @@
             [domina.xpath :refer [xpath]]
             [google-webmaster-tools-bulk-outdated-content-removal-clj.content-script.common :as common]
             [google-webmaster-tools-bulk-outdated-content-removal-clj.background.storage :refer [print-victims clear-victims!]]
-            [chromex.ext.runtime :as runtime :refer-macros [connect]]))
+            [chromex.ext.runtime :as runtime :refer-macros [connect]]
+            [chromex.ext.downloads :refer-macros [download]]
+            [chromex.ext.browser-action :refer-macros [set-badge-text]]
+            ))
 
+(def my-status (atom :done)) ;; :done or :running
 (def upload-chan (chan 1 (map (fn [e]
                                 (let [target (.-currentTarget e)
                                       file (-> target .-files (aget 0))]
@@ -34,7 +39,8 @@
                                                                            :error
                                                                            (into [])
                                                                            first)))
-          :else (prn "uncaught type: " type)
+          (= type :done) (do (prn "uncaught type: " type)
+                             (reset! my-status :done))
           )))
 
 (defn run-message-loop! [message-channel]
@@ -50,52 +56,78 @@
   (run-message-loop! background-port))
 
 (defn current-page []
-  (let []
-    [recom/v-box
-     :width "380px"
-     :align :center
-     :children [[:div {:style {:display "none"}}
-                 [:input {:id "bulkCsvFileInput" :type "file"
-                          :on-change (fn [e]
-                                       ;; (reset! my-status :running)
-                                       ;; (reset! cached-bad-victims-atom nil)
-                                       (put! upload-chan e))}]]
-                [recom/v-box
-                 :align :start
-                 :style {:padding "10px"}
-                 :children [[recom/title :label "Instructions:" :level :level1]
-                            [recom/label :label "- more instructions go here"]]
-                 ]
-                [recom/v-box
-                 :gap "10px"
-                 :children [[recom/button
-                             :label "Submit CSV File"
-                             :tooltip [recom/v-box
-                                       :children [[recom/label :label "Tooltip goes here"]]
-                                       ]
-                             :style {:width "200px"
-                                     :background-color "#007bff"
-                                     :color "white"}
-                             :on-click (fn [e] (-> "//input[@id='bulkCsvFileInput']" xpath single-node .click))]
-                            [recom/button
-                             :label "Clear cache"
-                             :style {:width "200px"}
-                             :on-click (fn [_]
-                                         (clear-victims!)
-                                         #_(set-badge-text #js{"text" ""})
-                                         #_(reset! cached-bad-victims-atom nil))]
-                            [recom/button
-                             :label "View cache"
-                             :style {:width "200px"}
-                             :on-click (fn [_]
-                                         (print-victims)
-                                         ;; TODO print bad-victims
-                                         )
-                             ]
-                            ;; TODO: do we need clear cache and view cache?
-                            ]
-                 ]]
-     ]))
+  (let [disable-error-download-ratom? (reaction (or (not= :done @my-status)
+                                                    (zero? (count @cached-bad-victims-atom))))
+        download-fn (fn []
+                      (let [content (-> @cached-bad-victims-atom csv-content csv/write-csv)
+                            data-blob (js/Blob. (clj->js [content])
+                                                (clj->js {:type "text/csv"}))
+                            url (js/URL.createObjectURL data-blob)]
+                        (download (clj->js {:url url
+                                            :filename "error.csv"
+                                            :saveAs true
+                                            :conflictAction "overwrite"
+                                            }))))]
+    (fn []
+      (let [cnt-ratom (reaction (count @cached-bad-victims-atom))]
+        [recom/v-box
+         :width "380px"
+         :align :center
+         :children [[:div {:style {:display "none"}}
+                     [:input {:id "bulkCsvFileInput" :type "file"
+                              :on-change (fn [e]
+                                           (reset! my-status :running)
+                                           (reset! cached-bad-victims-atom nil)
+                                           (put! upload-chan e))}]]
+                    [recom/v-box
+                     :align :start
+                     :style {:padding "10px"}
+                     :children [[recom/title :label "Instructions:" :level :level1]
+                                [recom/label :label "- more instructions go here"]]
+                     ]
+                    [recom/v-box
+                     :gap "10px"
+                     :children [[recom/button
+                                 :label "Submit CSV File"
+                                 :tooltip [recom/v-box
+                                           :children [[recom/label :label "Tooltip goes here"]]
+                                           ]
+                                 :style {:width "200px"
+                                         :background-color "#007bff"
+                                         :color "white"}
+                                 :on-click (fn [e] (-> "//input[@id='bulkCsvFileInput']" xpath single-node .click))]
+                                [recom/button
+                                 :label "Clear cache"
+                                 :style {:width "200px"}
+                                 :on-click (fn [_]
+                                             (clear-victims!)
+                                             (set-badge-text #js{"text" ""})
+                                             (reset! cached-bad-victims-atom nil))]
+                                [recom/button
+                                 :label "View cache"
+                                 :style {:width "200px"}
+                                 :on-click (fn [_]
+                                             (print-victims)
+                                             ;; TODO print bad-victims
+                                             )
+                                 ]]]
+                    [recom/h-box
+                     :children [[recom/title :label "Error Count: " :level :level2]
+                                [recom/title :label (str @cnt-ratom) :level :level2]]]
+
+                    [recom/button
+                     :label "Download Error CSV"
+                     :disabled? @disable-error-download-ratom?
+                     :tooltip [recom/v-box
+                               :children
+                               [[recom/label :label "Click here to download "]
+                                [recom/label :label "the list of URLs that errored out."]]]
+                     :style {:color            "white"
+                             :background-color  "#d9534f"
+                             :padding          "10px 16px"}
+                     :on-click download-fn]
+                    [recom/gap :size "30px"]]
+         ]))))
 
 (defn mount-root []
   (reagent/render [current-page] (.getElementById js/document "app")))
